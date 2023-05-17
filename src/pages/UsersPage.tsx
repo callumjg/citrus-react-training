dc17059e202cc50405af4e8349d836bb04314714
Callum Gibson <callum.gibson@citrusad.com>
2023-05-17 13:12:11 +1000

    04

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,25 +1,34 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 import UserDetail from "../components/UserDetail";
 import UsersTable from "../components/UsersTable";
 
-const UsersPage: React.FC = () => (
-  <Layout title="Users" subtitle="React users app">
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col">
-          <input className="form-control" />
+const UsersPage: React.FC = () => {
+  const [search, setSearch] = useState("");
+
+  return (
+    <Layout title="Users" subtitle="React users app">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col">
+            <input
+              className="form-control"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <UsersTable />
+          </div>
+          <div className="col">
+            <UserDetail />
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col">
-          <UsersTable />
-        </div>
-        <div className="col">
-          <UserDetail />
-        </div>
-      </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default UsersPage;
