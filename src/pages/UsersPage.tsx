2692179add40e63abe08fa8418dffa7140bf45db
Callum Gibson <callum.gibson@citrusad.com>
2023-05-17 12:58:50 +1000

    03

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,8 +1,24 @@
 import Layout from "../components/Layout";
+import UserDetail from "../components/UserDetail";
+import UsersTable from "../components/UsersTable";
 
 const UsersPage: React.FC = () => (
   <Layout title="Users" subtitle="React users app">
-    <div>Users Pages</div>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <input className="form-control" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <UsersTable />
+        </div>
+        <div className="col">
+          <UserDetail />
+        </div>
+      </div>
+    </div>
   </Layout>
 );
 
